1cf97b96bf0232766b0e0a37495c23fcdceab2b0
Peter Pistorius <peter.pistorius@gmail.com>
2021-07-24 22:13:34 +0200

    Fix tests.

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
+  testPathIgnorePatterns: ['fixtures', 'dist'],
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
